fa636c6afd534ff4afaf7c611fb1d87b61355ccc
Hongtao <hzhang784@wisc.edu>
2023-03-13 20:14:58 -0600

    finish rust version of flat combining and add some rust version benchmark

--- FlatCombining_rs/src/main.rs
+++ FlatCombining_rs/src/main.rs
@@ -1,12 +1,16 @@
 #![feature(sync_unsafe_cell)]
+#![feature(fn_traits)]
+#![feature(associated_type_bounds)]
 
-use std::cell::RefCell;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
 use flatcombining::FcLock;
 
+use crate::flatcombining::FcGuard;
+
 pub mod flatcombining;
+pub mod benchmark;
 
 // I have some magic semantics for some synchronization primitive!
 #[derive(Debug, Clone, Copy)]
@@ -16,48 +20,5 @@ unsafe impl Send for I32Unsafe {}
 unsafe impl Sync for I32Unsafe {}
 
 fn main() {
-    let counter = Arc::new(FcLock::new(0i64));
-
-    let mut handles = vec![];
-
-    let counter_mutex = Arc::new(Mutex::new(0i64));
-
-    for i in 0..128 {
-        let lock_ref = counter.clone();
-        let lock_ref_mutex = counter_mutex.clone();
-
-        let handle = thread::Builder::new().name(i.to_string()).spawn(move || {
-            // println!("Thread {} started", i);
-            for _ in 0..100000 {
-                lock_ref.lock(|mut guard| {
-                    for _ in 0..10000000 {
-                        // unsafe {
-                        //     *(counter_ref.0) += 1;
-                        // }
-                        *guard += 1;
-
-                        // let mut l = lock_ref_mutex.lock().unwrap();
-                        // (*l) += 1;
-                    }
-                });
-
-                // let mut counter = lock_ref_mutex.lock().unwrap();
-                // for _ in 0..10000000 {
-                //     (*counter) += 1;
-                // }
-            }
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.unwrap().join().unwrap();
-    }
-
-    counter.lock(|guard| {
-        let counter = *guard;
-        println!("{}", counter);
-    });
-
-    println!("{}", counter_mutex.lock().unwrap());
+    benchmark::benchmark();
 }
