6882d75a948aecada59e6dbd53345277a4dd2f17
Hongtao Zhang <hongtao_zhang@outlook.com>
2023-03-31 19:37:58 -0500

    add more change

---
+++ Rust_Implementation/src/operation.rs
@@ -0,0 +1,8 @@
+use crate::guard::Guard;
+
+pub(crate) struct Operation<T> {
+    pub f: *mut (dyn FnMut(&mut Guard<T>)),
+}
+
+unsafe impl<T> Sync for Operation<T> {}
+unsafe impl<T> Send for Operation<T> {}
