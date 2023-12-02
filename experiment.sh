3167c3a908b6a8c205a20e62be332780b72bb5b4
Hongtao <hongtao_zhang@outlook.com>
2023-12-01 18:24:41 -0600

    update experiment

--- experiment.sh
+++ experiment.sh
@@ -6,4 +6,6 @@ cargo build --release
 target/release/dlock -e response-time-single-addition -t 2 4 8 16 32 64 128 -d 5
 target/release/dlock -e response-time-ratio-one-three -t 2 4 8 16 32 64 128 -d 5
 
+cd ..
+
 tar --zstd -cf output.tar.zst visualization/output
