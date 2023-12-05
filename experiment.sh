7cfb5bbb605783b7a32829e934bffd73e97b65dd
Hongtao <hongtao_zhang@outlook.com>
2023-12-05 12:23:00 -0600

    update script

--- experiment.sh
+++ experiment.sh
@@ -3,8 +3,8 @@ cd rust
 cargo build --release
 
 # target/release/dlock -e counter-ratio-one-three -d 30
-target/release/dlock counter-proportional -t 32 -d 3 --stat-response-time
-target/release/dlock response-time-single-addition -t 32 -d 3 --stat-response-time
+target/release/dlock counter-proportional -t 32 64 --stat-response-time
+target/release/dlock response-time-single-addition -t 32 64 --stat-response-time
 
 
 cd ..
