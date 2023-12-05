7d5efdd7cd61abf9662bcaafdab810f2257e7c7c
Hongtao <hongtao_zhang@outlook.com>
2023-12-05 15:56:13 -0600

    update experiment scripts

--- experiment.sh
+++ experiment.sh
@@ -5,8 +5,3 @@ cargo build --release
 # target/release/dlock -e counter-ratio-one-three -d 30
 target/release/dlock counter-proportional -t 32 64 --stat-response-time
 target/release/dlock response-time-single-addition -t 32 64 --stat-response-time
-
-
-cd ..
-
-tar --zstd -cf output.tar.zst visualization/output
