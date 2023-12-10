ce61a8a98e7ff2bd665d3b03e603a6fca31f9380
Hongtao <hongtao_zhang@outlook.com>
2023-12-09 19:30:53 -0600

    fix typo

--- rust/src/command_parser/experiment.rs
+++ rust/src/command_parser/experiment.rs
@@ -13,7 +13,7 @@ pub enum Experiment {
         cs_durations: Vec<Duration>,
         #[arg(value_parser = parse_duration, long = "non-cs", default_value = "0", value_delimiter = ',')]
         non_cs_durations: Vec<Duration>,
-        #[arg(long = "file-name", default_value = "proposition_counter")]
+        #[arg(long = "file-name", default_value = "proportional_counter")]
         file_name: String,
     },
     ResponseTimeSingleAddition,
