29fe4f71fe5504034864849729acc97cdc59d978
Hongtao <hongtao_zhang@outlook.com>
2023-12-09 19:07:49 -0600

    small revision to make safer code

--- rust/src/benchmark/one_three_ratio_counter.rs
+++ rust/src/benchmark/one_three_ratio_counter.rs
@@ -6,7 +6,7 @@ use super::proposion_counter;
 pub fn counter_one_three_benchmark() -> Box<dyn Fn(LockBenchInfo<u64>)> {
     let cs_durations = vec![Duration::from_micros(10), Duration::from_micros(30)];
 
-    let non_cs_durations = vec![];
+    let non_cs_durations = vec![Duration::ZERO];
 
     proposion_counter::counter_proportional(cs_durations, non_cs_durations)
 }
